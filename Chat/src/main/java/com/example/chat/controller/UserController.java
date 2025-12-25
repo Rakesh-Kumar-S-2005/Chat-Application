74f435f67c63f6b3e381561d19232a82acc91c02
Rakesh-Kumar-S-2005 <230097.it@rmkec.ac.in>
2025-12-25 22:04:35 +0530

    userlist added

--- Chat/src/main/java/com/example/chat/controller/UserController.java
+++ Chat/src/main/java/com/example/chat/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class UserController {
     @GetMapping("/test")
     public String test(){
         return "test";
